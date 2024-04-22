c3ff17daf52bcb2cb8d2ee8213980ab831f55f25
robert0004 <6243646+robert0004@users.noreply.github.com>
2024-04-22 09:12:02 -0700

    fix markdown

--- app/Main.hs
+++ app/Main.hs
@@ -51,7 +51,7 @@ replyMessage :: Message -> Text -> DiscordHandler ()
 replyMessage oldMsg newMsg = void $ restCall (R.CreateMessage (messageChannelId oldMsg) newMsg)
 
 pointFreeHandler :: Message -> Text -> DiscordHandler ()
-pointFreeHandler msg content = replyMessage msg $ "```hs" <> (pack $ show (pointfree $ unpack content)) <> "```"
+pointFreeHandler msg content = replyMessage msg $ "```hs\n" <> (pack $ show (pointfree $ unpack content)) <> "```"
 
 haskellEvalHandler :: Message -> Text -> DiscordHandler ()
 haskellEvalHandler msg content = do
